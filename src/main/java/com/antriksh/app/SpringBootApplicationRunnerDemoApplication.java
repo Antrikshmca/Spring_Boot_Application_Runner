692a87db6b0bb49b4d7f1a0765a96f618c56fd53
Antriksh <Antriksh@DESKTOP-U9R8BMF>
2020-10-02 11:50:36 +0530

    This is spring boot apllication Line runner example.

---
+++ src/main/java/com/antriksh/app/SpringBootApplicationRunnerDemoApplication.java
@@ -0,0 +1,21 @@
+package com.antriksh.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.antriksh.app.servre.MessageServiceProvider;
+
+@SpringBootApplication
+public class SpringBootApplicationRunnerDemoApplication {
+
+	public static void main(String[] args) {
+
+		ConfigurableApplicationContext ctx = SpringApplication.run(SpringBootApplicationRunnerDemoApplication.class, args);
+		MessageServiceProvider messageServiceProvider = ctx.getBean(MessageServiceProvider.class);
+		
+		String message = messageServiceProvider.message();
+		System.out.println(message);
+	
+	}
+}
